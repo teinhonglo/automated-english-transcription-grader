56af41897d88903ba7a8d579b9352a359c12cc2a
teinhonglo <teinhonglo@gmail.com>
2022-12-14 11:35:35 +0800

    [Code] Added multi-column within-0.5/1.0 accuracy

--- local/metrics_cpu.py
+++ local/metrics_cpu.py
@@ -9,6 +9,8 @@ def compute_metrics(total_losses, all_score_predictions, all_score_targets):
     total_losses['pearson'] = stats.pearsonr(all_score_predictions, all_score_targets)[0]
     total_losses['within_0.5'] = _accuracy_within_margin(all_score_predictions, all_score_targets, 0.5)
     total_losses['within_1'] = _accuracy_within_margin(all_score_predictions, all_score_targets, 1)
+    total_losses['mcwithin_0.5'] = compute_within_acc(all_score_predictions, all_score_targets, 0.5)
+    total_losses['mcwithin_1'] = compute_within_acc(all_score_predictions, all_score_targets, 1)
 
 def _accuracy_within_margin(score_predictions, score_target, margin):
     """ Returns the percentage of predicted scores that are within the provided margin from the target score. """
@@ -34,3 +36,15 @@ def compute_mcrmse(all_score_predictions, all_score_targets):
     
     return score_rmse
 
+def compute_within_acc(all_score_predictions, all_score_targets, margin):
+    unique_classes = np.unique(all_score_targets)
+    num_classes = len(unique_classes)
+    score_rmse = 0.
+    
+    for c in unique_classes:
+        indices = np.where(all_score_targets == c)
+        score_predictions = all_score_predictions[indices]
+        score_targets = all_score_targets[indices]
+        score_rmse += 1 / num_classes * _accuracy_within_margin(score_predictions, score_targets, margin)
+    
+    return score_rmse
